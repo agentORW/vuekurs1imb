91104b033a737c16c7ee82b963118738056cb510
agentORW <oskar.wisnes@gmail.com>
2025-03-05 15:25:50 +0100

    Initial push

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import PictureBox from './components/Picture.vue'
+import reactImagePath from './assets/arireact.png'
+
+const reactImage = ref(reactImagePath)
+</script>
+
+<template>
+  <PictureBox 
+  :imageSrc="reactImage" 
+  imageAlt="React Logo"/>
+  <h1>Yeah</h1>
+</template>
+
+<style>
+</style>
---
+++ src/components/Picture.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- <div class="picture-box">
+        <img :src="`./assets/${image}`" alt="Picture" class="picture-box__image" />
+        <h2 class="picture-box__title">{{ title }}</h2>
+        <p class="picture-box__description">{{ description }}</p>
+    </div> -->
+    <h1>{{ title }}</h1>
+    <img 
+      :src="props.imageSrc" 
+      :alt="props.imageAlt" 
+      class="picture"
+    />
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  },
+  imageAlt: {
+    type: String,
+    default: 'Image description'
+  }
+})
+</script>
+
+<style scoped>
+.picture-box {
+    border: 1px solid #ccc;
+    padding: 16px;
+    text-align: center;
+}
+
+.picture-box__image {
+    max-width: 100%;
+    height: auto;
+}
+
+.picture-box__title {
+    font-size: 1.5em;
+    margin: 16px 0 8px;
+}
+
+.picture-box__description {
+    font-size: 1em;
+    color: #666;
+}
+</style>
